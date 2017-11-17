b369df87f96790396493090e6f6cb26535eb128c
ahmedelgohary <amdelgohary@gmail.com>
2017-11-17 16:47:55 -0700

    add time exchange

--- mainClockScreen.cpp
+++ mainClockScreen.cpp
@@ -2,13 +2,43 @@
 #include <SPI.h>
 #include <SD.h>
 #include <Adafruit_ILI9341.h>
+#include "sendTime.h"
+
+// initiate time array, to be filled in the format hhmmss
+int time[6] = {0};
+// i and read are used to make sure we read all 6 digits of time.
+int i = 0;
+bool read = 0;
 
 void setup(){
+	init();
+	Serial.begin(9600);
 
+}
+
+void loop(){
+  while (!read){
+    if(Serial.available()>0){
+      int temp = Serial.read();
+      if(temp != ','){
+          int num = temp - 48;
+          time[i] = num;
+          i++;
+      }
+    }
+    if (i == 6){read = 1;}
+  }
 }
 
 int main(){
 	setup();
-	// do stuff :)
+	sendTime();
+	loop();
+
+	for (int i = 0; i < 6; i++){
+		Serial.print(time[i];)
+	}
+
+	Serial.end();
 	return 0;
 }
