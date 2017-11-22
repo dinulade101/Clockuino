46f9e9bbe50476f6e4f313ea25f3a56a1fa53e76
dinulade101 <cmput274@ualberta.ca>
2017-11-21 23:27:53 -0700

    merged copy

--- mainClockScreen.cpp
+++ mainClockScreen.cpp
@@ -1,6 +1,4 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <SD.h>
 #include <Adafruit_ILI9341.h>
 
 
@@ -10,9 +8,53 @@ int time[6] = {0};
 int i = 0;
 bool read = 0;
 
+#define TFT_DC 9
+#define TFT_CS 10
+
+#define fontSize 11
+#define digitWidth 5*fontSize
+#define spacingBetweenDigits 15
+
+bool colonState = false;
+
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
 void setup(){
 	init();
 	Serial.begin(9600);
+}
+
+void setNumOf7SegDisplay(int digitVal, int digitLoc, int cursorStart){
+	tft.setCursor((digitLoc*(digitWidth+spacingBetweenDigits)+cursorStart), 15);
+	tft.setTextSize(fontSize);
+	tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+	tft.print(digitVal);
+}
+
+void setColon(){
+	tft.setCursor(120, 15);
+	tft.setTextSize(fontSize);
+	if (colonState){
+		tft.setTextColor(ILI9341_BLACK, ILI9341_BLACK);
+		colonState = false;
+	}
+	else{
+		colonState = true;
+		tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+	}
+	tft.print(":");
+}
+
+void initializeFour7SegDisplays(){
+	for (int j=0; j<8; j++){
+		for (int i=0; i<2; i++){
+			setNumOf7SegDisplay(j, i, 0);
+		}
+		for (int i=2; i<4; i++){
+			setNumOf7SegDisplay(j+1, i, spacingBetweenDigits*2);
+		}
+	}
 
 }
 
@@ -32,10 +74,21 @@ void loop(){
 
 int main(){
 	setup();
-	loop();
+
+	tft.begin();
+	tft.fillScreen(ILI9341_BLACK);
+	tft.setRotation(3);
+	initializeFour7SegDisplays();
+
+	while (true){
+		setColon();
+		delay(100);
+	}
+
+	//loop();
 
 	for (int i = 0; i < 6; i++){
-		Serial.print(time[i];)
+		Serial.print(time[i]);
 	}
 
 	Serial.end();
